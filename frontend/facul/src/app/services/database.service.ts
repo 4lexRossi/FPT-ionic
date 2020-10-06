b2b922528f81bfeeac360cb145c6d2dbc6c5684c
4lexRossi <devalexrossi@gmail.com>
2020-10-06 20:06:59 -0300

    database mongo

--- frontend/facul/src/app/services/database.service.ts
+++ frontend/facul/src/app/services/database.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  baseUrl = 'https://pokeapi.co/api/v2';
+  imageUrl = 'https //raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+  getUser(offset = 0){
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(
+      map( result => {
+        return result['results'];
+      })
+    )
+  }
+  getUserImage(index) {
+    return `${this.imageUrl}${index}.png`;
+  }
 }
